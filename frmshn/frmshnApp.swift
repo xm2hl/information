5b8c76a1795874dd300fe82ca9ed9acf3c858189
mamdouh al rashidi <mamdouh_m@outlook.sa>
2023-05-16 10:13:44 +0300

    Initial Commit

---
+++ frmshn/frmshnApp.swift
@@ -0,0 +1,17 @@
+//
+//  frmshnApp.swift
+//  frmshn
+//
+//  Created by Apple on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct frmshnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
